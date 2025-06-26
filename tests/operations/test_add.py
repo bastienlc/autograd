d0471894b9491643f168a492e580db4ac5a36130
bastienlc <78959054+bastienlc@users.noreply.github.com>
2025-06-26 21:27:43 +0200

    Add some tests

--- tests/operations/test_add.py
+++ tests/operations/test_add.py
@@ -10,7 +10,7 @@ n = 5
 m = 10
 
 
-def test_add_forward():
+def test_add_forward_1():
     shape = (n, m)
     a = np.random.randn(*shape)
     b = np.random.randn(*shape)
@@ -19,12 +19,41 @@ def test_add_forward():
     assert np.allclose(a + b, result, atol=1e-6, rtol=1e-6)
 
 
-def test_add_backward():
+def test_add_forward_2():
+    a = np.random.randn(n, m)
+    b = np.random.randn(m)
+    result = (Tensor.from_numpy(a) + Tensor.from_numpy(b)).to_numpy()
+
+    assert np.allclose(a + b[None, :], result, atol=1e-6, rtol=1e-6)
+
+
+def test_add_backward_1():
     shape = (n, m)
 
     # torch implementation
     a1 = torch.randn(*shape, requires_grad=True)
     b1 = torch.randn(*shape, requires_grad=True)
+    c1 = a1 + b1
+    grad1 = torch.ones_like(c1)
+    c1.backward(grad1)
+
+    # autograd implementation
+    a2 = Tensor.from_torch(a1, requires_grad=True)
+    b2 = Tensor.from_torch(b1, requires_grad=True)
+
+    c2 = a2 + b2
+    grad2 = Tensor.from_torch(grad1)
+    c2.backward(grad2)
+
+    # Check gradients
+    assert torch.allclose(a1.grad, a2.get_grad().to_torch())
+    assert torch.allclose(b1.grad, b2.get_grad().to_torch())
+
+
+def test_add_backward_2():
+    # torch implementation
+    a1 = torch.randn((n, m), requires_grad=True)
+    b1 = torch.randn((m,), requires_grad=True)
     c1 = a1 + b1
     grad1 = torch.ones_like(c1)
     c1.backward(grad1)
--- tests/operations/test_mul.py
+++ tests/operations/test_mul.py
@@ -10,7 +10,7 @@ n = 5
 m = 10
 
 
-def test_mul_forward():
+def test_mul_forward_1():
     shape = (n, m)
     a = np.random.randn(*shape)
     b = np.random.randn(*shape)
@@ -19,7 +19,23 @@ def test_mul_forward():
     assert np.allclose(a * b, result, atol=1e-6, rtol=1e-6)
 
 
-def test_mul_backward():
+def test_mul_forward_2():
+    a = np.random.randn(n, m)
+    b = np.random.randn(m)
+    result = (Tensor.from_numpy(a) * Tensor.from_numpy(b)).to_numpy()
+
+    assert np.allclose(a * b[None, :], result, atol=1e-6, rtol=1e-6)
+
+
+def test_mul_forward_3():
+    shape = (n, m)
+    a = np.random.randn(*shape)
+    result = (Tensor.from_numpy(a) * Tensor.from_numpy(a)).to_numpy()
+
+    assert np.allclose(a * a, result, atol=1e-6, rtol=1e-6)
+
+
+def test_mul_backward_1():
     shape = (n, m)
 
     # torch implementation
@@ -40,3 +56,44 @@ def test_mul_backward():
     # Check gradients
     assert torch.allclose(a1.grad, a2.get_grad().to_torch())
     assert torch.allclose(b1.grad, b2.get_grad().to_torch())
+
+
+def test_mul_backward_2():
+    # torch implementation
+    a1 = torch.randn((n, m), requires_grad=True)
+    b1 = torch.randn((m,), requires_grad=True)
+    c1 = a1 * b1
+    grad1 = torch.ones_like(c1)
+    c1.backward(grad1)
+
+    # autograd implementation
+    a2 = Tensor.from_torch(a1, requires_grad=True)
+    b2 = Tensor.from_torch(b1, requires_grad=True)
+
+    c2 = a2 * b2
+    grad2 = Tensor.from_torch(grad1)
+    c2.backward(grad2)
+
+    # Check gradients
+    assert torch.allclose(a1.grad, a2.get_grad().to_torch())
+    assert torch.allclose(b1.grad, b2.get_grad().to_torch())
+
+
+def test_mul_backward_3():
+    shape = (n, m)
+
+    # torch implementation
+    a1 = torch.randn(*shape, requires_grad=True)
+    b1 = a1 * a1
+    grad1 = torch.ones_like(b1)
+    b1.backward(grad1)
+
+    # autograd implementation
+    a2 = Tensor.from_torch(a1, requires_grad=True)
+
+    b2 = a2 * a2
+    grad2 = Tensor.from_torch(grad1)
+    b2.backward(grad2)
+
+    # Check gradients
+    assert torch.allclose(a1.grad, a2.get_grad().to_torch())
